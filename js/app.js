025a88a3e274e355c8d09bdbd166afa7de4d3050
Viet Anh Le <mc.vietanh@gmail.com>
2021-04-22 13:07:58 -0400

    done

--- js/app.js
+++ js/app.js
@@ -2,5 +2,24 @@
 
 // Make sure all DOM did fully load
 window.addEventListener('load', (event) => {
-  console.log(1);
+  // Call the coffeeOrder method to output your coffee order to the screen as text inside the paragraph element.
+  // createContent();
 });
+
+var employee = {
+  name: ['Biff', 'Henderson'],
+  employeeNumber: 30573,
+  department: 'Reponsessions',
+  interests: ['the Mob', 'fast cars', 'baseball'],
+  active: true,
+};
+// Write a short script that creates an interesting sentence by concatenating members of the above employee object together (using either dot notation or bracket notation). Include as much information about the employee as you can. Output the resulting content as text inside the paragraph element.
+const para = document.querySelector('p');
+
+for (let prop in employee) {
+  const contentText = document.createElement('span');
+  let contentPara = prop + ':' + ' ' + employee[prop] + '. ';
+
+  contentText.textContent = contentPara;
+  para.appendChild(contentText);
+}
